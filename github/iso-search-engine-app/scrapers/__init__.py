aafb5479710c629bcb4425b804fafbaab781c4f1
mint <atxxx@duck.com>
2025-05-04 23:18:54 -0500

    Auto-sync: Files changed on 2025-05-04 22:57:01

---
+++ github/iso-search-engine-app/scrapers/__init__.py
@@ -0,0 +1,7 @@
+# ISO Search Engine Scrapers Package
+# This file makes the directory a Python package
+
+from . import official_linux
+from . import official_windows
+from . import distrowatch
+from . import linuxtracker
